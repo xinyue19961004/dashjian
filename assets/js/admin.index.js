e9364a5072e5fae9a8d90df665d374e3762092e8
xinyue <3059096258@qq.com>
2020-09-05 17:14:10 +0800

    提交所有

---
+++ assets/js/admin.index.js
@@ -0,0 +1,65 @@
+$(function () {
+    getUserInfo()
+    function getUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            // headers 就是请求头配置对象
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || ''
+            // },
+            success: function (res) {
+
+                console.log(res);
+                if (res.status !== 0) {
+                    layui.layer.msg('获取用户信息失败！')
+                }
+                // 调用 renderAvatar 渲染用户的头像
+                renderAvatar(res.data)
+            },
+
+        })
+    }
+    function renderAvatar(user) {
+        //1.获取用户的名称 data: {id: 3669, username: "wxy", nickname: "燕燕美", email: "wxy@qq.com", user_pic: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQA…dElnoeXmCil5p+73K/wMCSu6TcRKHrwAAAABJRU5ErkJggg=="}
+        message: "获取用户基本信息成功！"
+        status: 0
+        var name = user.nickname || user.username
+        //2.设置欢迎文本
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        //3.按需渲染用户的头像
+        if (user.user_pic != null) {
+            //3.1 渲染图片头像
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            //3.2渲染文本头像
+            $('.layui-nav-img').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html('first')
+        }
+
+    }
+
+    //点击退出按钮，实现退出功能,清空内存，并跳转至首页
+    var layer = layui.layer
+
+
+    $('#btnLogout').on('click', function () {
+        alert('123')
+        // 提示用户是否确认退出.layui提供的方法
+        layer.msg("kkk")
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+            //重新跳转到登录页面
+            location.href = '/login.html'
+            //清空本地存储中的token
+            layer.close(index)
+
+        })
+
+        //关闭confirm询问框
+
+
+    })
+})
---
+++ assets/js/admin.index1.js
@@ -0,0 +1,54 @@
+$(function () { 
+    getUserInfo()
+    function getUserInfo() {
+        var layer=layui.layer
+        $.ajax({
+            type: "GET",
+            url: "/my/userinfo",
+            //请求头携带token
+            headers: {Authorization:localStorage.getItem('token')||''},
+            success: function (res) {
+                if (res.status != 0) {
+                   layer.message('获取用户信息失败')
+                }
+                //渲染用户信息
+                renderAvatar(res.data)
+            },
+            complete: function (options) {
+                if (options.responseJson.status == 1 && options.responseJson.message == '身份验证失败') {
+                    //强制清空token
+                    localStorage.removeItem('token')
+                    location.href='/login.html'
+                }
+                
+            }
+        });
+    }
+    //渲染头像函数
+    function renderAvatar(user) {
+        var name = user.nickname || user.username
+        //设置欢迎文本
+        $('#welcome').html(`欢迎&nbsp;{$name}`)
+        //按需渲染用户头像
+        if (user.user_pic != null) {
+            //文本头像隐藏
+            $('.text-avatar').hide()
+            //渲染图片头像
+            $('.layui-nav-img"').prop('scr', user.user_pic).show()
+        } else {
+            $('.layui-nav-img"').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html('first')
+        }
+    }
+    //点击退出按钮，实现退出功能，清空内存，并跳转至首页
+    var layer = layui.layer
+    $('#btnLogout').on('click', function () { 
+        layer.confirm('确定退出登录', {icon: 3, title:'提示'}, function(index){
+             //关闭confirm询问框
+             localStorage.removeItem('token')
+             location.href='/login.html'
+            layer.close(index);
+          });
+     })
+ })
